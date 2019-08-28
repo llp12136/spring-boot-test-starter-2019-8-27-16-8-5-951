af6d018abee15bd05b5d501c45f44b92d2a96207
llp12136 <liliping@zybank.com.cn>
2019-08-28 19:34:56 +0800

    get and post commit

---
+++ src/main/java/com/oocl/web/sampleWebApp/userController.java
@@ -0,0 +1,30 @@
+package com.oocl.web.sampleWebApp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class userController {
+    private static List<User> users = new ArrayList(){{
+        add(new User("001","xiaoming"));
+       // add(new User("002","xiaohong"));
+    }};
+
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        users.add(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> queryUser() {
+        return ResponseEntity.ok(users);
+    }
+}
--- src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
+++ src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
@@ -1,16 +1,74 @@
 package com.oocl.web.sampleWebApp;
 
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SampleWebAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
+
+	@Autowired
+	private MockMvc mockMvc;
+
+//	@Test
+//	public void getTest() throws Exception {
+//		this.mockMvc.perform(get("/user")).andDo(print()).andExpect(status().isOk())
+//				.andExpect(content().string(containsString("Hello World")));
+//	}
+//
+//	@Test
+//	public void postTest() throws Exception {
+//		this.mockMvc.perform(post("/user")).andDo(print()).andExpect(status().isOk())
+//				.andExpect(content().string(containsString("Hello World")));
+//	}
+//post
+	@Test
+	public void should_return_201_status_when_create_user() throws Exception {
+		//Given
+		MockHttpServletRequestBuilder input = post("/user").content("{\"username\":\"xiaoming\"}").contentType(MediaType.APPLICATION_JSON);
+		//When
+		ResultActions result = mockMvc.perform(input);
+		//Then
+		result
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isCreated());
+	}
+//GET
+	@Test
+	public void should_return_ok_and_content_when_query_all_user() throws Exception {
+		//Given
+		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/user");
+		//When
+		ResultActions performResult = mockMvc.perform(requestBuilder);
+		//Then
+		performResult
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("[{\"id\":\"001\",\"name\":\"xiaoming\"}]"));
+	}
 }
